c6b457588bf769218bdd7c0e85f7094e2d3a4cdc
Everton Freitas Xavier da Silva <t_everton.silva+hapvida@hapvida.com.br>
2026-09-20 13:08:44 -0300

    chore: update global style

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@/index.css'
+import '@/presentation/styles/global.css'
 import { Router } from '@/main/routes/router'
 
 const container = document.getElementById('root') as HTMLElement
