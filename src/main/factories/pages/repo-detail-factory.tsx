4c81375821a5578936ddb590104b81ccca44ed21
Everton Freitas Xavier da Silva <t_everton.silva+hapvida@hapvida.com.br>
2026-09-20 13:24:34 -0300

    chore: improve identation

--- src/main/factories/pages/repo-detail-factory.tsx
+++ src/main/factories/pages/repo-detail-factory.tsx
@@ -1,4 +1,6 @@
-import { RepoDetail } from '@/presentation/pages/repo-detail'
-import { makeRemoteLoadGithubRepo } from '@/main/factories/usecases'
+import { RepoDetail } from "@/presentation/pages/repo-detail";
+import { makeRemoteLoadGithubRepo } from "@/main/factories/usecases";
 
-export const makeRepoDetail = () => <RepoDetail loadGithubRepo={makeRemoteLoadGithubRepo()} />
+export const makeRepoDetail = () => (
+  <RepoDetail loadGithubRepo={makeRemoteLoadGithubRepo()} />
+);
