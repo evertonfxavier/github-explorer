c40dbc2b24da9dec7bc354c1e522b43ddc050676
Everton Freitas Xavier da Silva <t_everton.silva+hapvida@hapvida.com.br>
2026-09-20 13:19:35 -0300

    refactor: revert axios http client factory to raw, undecorated client

--- src/main/factories/http/axios-http-client-factory.ts
+++ src/main/factories/http/axios-http-client-factory.ts
@@ -1,6 +1,3 @@
 import { AxiosHttpClient } from '@/infra/http'
-import type { HttpClient } from '@/data/protocols/http'
-import { AuthorizeHttpClientDecorator } from '@/main/decorators'
 
-export const makeAxiosHttpClient = <R = unknown>(): HttpClient<R> =>
-  new AuthorizeHttpClientDecorator<R>(new AxiosHttpClient<R>(), import.meta.env.VITE_GITHUB_API_TOKEN)
+export const makeAxiosHttpClient = <R = unknown>(): AxiosHttpClient<R> => new AxiosHttpClient<R>()
