fc66fefb2b0a686c22ea50f8b8c4268119cd613e
Everton Freitas Xavier da Silva <t_everton.silva+hapvida@hapvida.com.br>
2026-09-20 12:51:02 -0300

    feat: add field validation layer (RequiredFieldValidation, ValidationComposite)

---
+++ src/validation/validators/required-field-validation.ts
@@ -0,0 +1,14 @@
+import type { FieldValidation } from '@/validation/protocols'
+import { RequiredFieldError } from '@/validation/errors'
+
+export class RequiredFieldValidation implements FieldValidation {
+  field: string
+
+  constructor(field: string) {
+    this.field = field
+  }
+
+  validate(fieldValue: string): Error | undefined {
+    return fieldValue ? undefined : new RequiredFieldError(this.field)
+  }
+}
