e2176a98bca4619039255d2c46e8dd2dc4d3b19d
Everton Freitas Xavier da Silva <t_everton.silva+hapvida@hapvida.com.br>
2026-09-20 14:05:32 -0300

    feat: add global footer, set Inter as primary font and install react icons lib

--- src/presentation/pages/search/search.tsx
+++ src/presentation/pages/search/search.tsx
@@ -10,7 +10,7 @@ export function Search({ validation }: Props) {
   const navigate = useNavigate()
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-6 px-4">
+    <main className="flex h-full flex-col items-center justify-center gap-6 px-4 py-10">
       <h1 className="text-2xl font-semibold">GitHub User View</h1>
       <SearchForm validation={validation} onSearch={username => navigate(`/users/${username}`)} />
     </main>
